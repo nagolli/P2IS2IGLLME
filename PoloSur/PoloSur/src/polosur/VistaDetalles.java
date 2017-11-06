6f724bda9d67f30d6626265420317867eaa46aed
LimoneUV <limone@alumni.uv.es>
2017-11-06 16:58:01 +0100

    Vista mejorada

--- PoloSur/PoloSur/src/polosur/VistaDetalles.java
+++ PoloSur/PoloSur/src/polosur/VistaDetalles.java
@@ -74,6 +74,7 @@ public class VistaDetalles extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jLabel1.setText("Fecha:");
 
