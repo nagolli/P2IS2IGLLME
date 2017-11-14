ca42e18ef15f7bf138cd9d78d43e1443199a1e88
nagolli <nacho.gomis@gmail.com>
2017-11-14 11:13:24 +0100

    Inicio Practica 3 y entregable del 2

---
+++ IS2Pr4/src/P4/HistoricoContenedores.java
@@ -0,0 +1,107 @@
+package P4;
+/**
+ * Clase HistoricoContenedores que 
+ * permite gestionar los contenedores mediante un vector.
+ * 
+ */
+import java.util.Vector;
+
+public class HistoricoContenedores {
+	private Vector<Contenedor2> vecContenedores;
+	private int contador;
+	
+	/**
+	 * Constructor sin parametros
+	 * @param vecContenedores, vector de maximo 100 objetos tipo Contenedor2
+	 * @param contador, un entero para llevar la cuenta del numero de contenedores activos
+	 */
+	public HistoricoContenedores(){
+		vecContenedores = new Vector<Contenedor2>(100);
+		this.contador = 0;
+	}
+	
+	/**
+	 * Metodo para añadir un Contenedor2 al vector
+	 * @param c4, objeto tipo Contenedor2
+	 */
+	public void sumarContenedor(Contenedor2 c4)
+	{
+		vecContenedores.add(c4);
+		contador++;
+	}
+	
+	/**
+	 * Metodo para eliminar el último Contenedor2 añadido al vector
+	 */
+	public void eliminarContenedor()
+	{
+		vecContenedores.remove(contador);
+		contador--;
+	}
+
+	/**
+	 * Metodo para añadir un paquete al ultimo contenedor introducido
+	 * @param p2, un objeto de tipo Paquete
+	 */
+	public void sumarPaquete(Paquete p2)
+	{
+		vecContenedores.elementAt(contador).sumarPaquete(p2);
+	}
+	
+	/**
+	 * @return el numero de contenedores activos actualmente
+	 */
+	public int numeroContenedores()
+	{
+		return contador;
+	}
+	
+	/**
+	 * Método que devuelve el último contenedor introducido
+	 * @return el último contenedor introducido
+	 */
+	public Contenedor2 ultimoContenedor()
+	{
+		return vecContenedores.elementAt(vecContenedores.size() - 1);
+	}
+	
+	/**
+	 * Metodo para calcular el contenedor mas grande
+	 * @return el contenedor mas grande
+	 */
+	public Contenedor2 contenedorMasGrande()
+	{
+		int areaMax = 0;
+		int posicion = 0;
+		for(int i=0; i<vecContenedores.size(); i++)
+		{
+			if(areaMax < vecContenedores.elementAt(i).calcularArea())
+			{
+				areaMax = vecContenedores.elementAt(i).calcularArea();
+				posicion = i;
+			}
+		}
+		
+		return vecContenedores.elementAt(posicion);
+	}
+	
+	/**
+	 * Metodo para calcular el contenedor mas pequeño
+	 * @return el contenedor mas pequeño
+	 */
+	public Contenedor2 contenedorMasPequeno()
+	{
+		int areaMin = vecContenedores.elementAt(0).calcularArea();
+		int posicion = 0;
+		for(int i=0; i<vecContenedores.size(); i++)
+		{
+			if(areaMin > vecContenedores.elementAt(i).calcularArea())
+			{
+				areaMin = vecContenedores.elementAt(i).calcularArea();
+				posicion = i;
+			}
+		}
+		
+		return vecContenedores.elementAt(posicion);
+	}
+}
